479fd5b75b6ccebdf10e8303ae9f893dfca906ba
Abhishekranjan-01 <abhishekranjan7991@outlook.com>
2023-09-18 00:04:55 +0530

    Working on Article Content Fetching

--- 1-Level/News-Aggregator/script.js
+++ 1-Level/News-Aggregator/script.js
@@ -2,10 +2,27 @@ const API = fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=bc0d1a
 
 const articleTitle = document.getElementById('title');
 
+const newsImage = document.getElementById('news-image');
+
+const newsSource = document.getElementById('source');
+
+const publishedAt = document.getElementById('date-time');
+
+const newsArticle = document.querySelector('article');
+
 API.then((response) => response.json())
         .then((data) => {
             console.log(data);
             articleTitle.textContent = data['articles'][0]['title'];
 
-            console.log(articleTitle);
+            newsImage.src = data['articles'][0]['urlToImage'];
+
+            newsSource.textContent = data['articles'][0]['source']['name'];
+
+            publishedAt.textContent = new Date(`${data['articles'][0]['publishedAt']}`).toLocaleString() ;
+
+            newsArticle.textContent = data['articles'][0]['content'];
+
+            // return(data['articles'][0]['content']);
         })
+           
