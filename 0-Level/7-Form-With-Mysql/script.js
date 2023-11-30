7d5b1f24ea2152d103ca865a462c2be98fd3c219
Abhishekranjan-01 <abhishekranjan7991@outlook.com>
2023-11-30 23:53:55 +0530

    Database Connectivity Done SuccessFully

--- 0-Level/7-Form-With-Mysql/script.js
+++ 0-Level/7-Form-With-Mysql/script.js
@@ -1,12 +1,39 @@
+
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
   host: "localhost",
-  user: " root@localhost",
+  user: "root",
   password: "mysql@79911"
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+try{
+  console.log("11");
+  con.connect((err)=> {
+    if (err) {
+      throw err;
+    }
+
+    
+    console.log("1");
+    console.log("Connected!");
+  });
+console.log("111");
+}
+
+catch(err)
+{
+  console.log("2");
+  console.log(err);
+}
+finally{
+  console.log("3");
+
+      con.end();
+      console.log("something");
+    
+ 
+  console.log("4");
+  console.log("Disconnected !");
+  console.log("5");
+}
