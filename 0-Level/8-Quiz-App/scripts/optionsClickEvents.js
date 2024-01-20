d0dfdd9d9a2a36ff6e0dc74f206d4fcee0c3b05d
Abhishekranjan-01 <abhishekranjan7991@outlook.com>
2024-01-20 23:25:42 +0530

    Created Functions For Adding Events In Questions's options

---
+++ 0-Level/8-Quiz-App/scripts/optionsClickEvents.js
@@ -0,0 +1,104 @@
+import {scoreCount,scoreCountDisplay,decreaseScore,increaseScore,questionOne,questionTwo,questionThree,questionFour,questionFive,questionSix,questionSeven,questionEight,questionNine,questionTen} from "./checkAnswers.js";
+
+
+function addEventsInQuestionOne(){
+    const QuestionOneOptions = document.querySelectorAll("[data-container='Container For Questions And Options'] li");
+
+    for(let option of QuestionOneOptions)
+    {
+        option.addEventListener("click",questionOne);
+    }
+}
+
+function addEventsInQuestionTwo(){
+
+
+
+    const QuestionTwoOptions = document.querySelectorAll("[data-questions='Options_for_Q-2'] li");
+
+    console.log(QuestionTwoOptions);
+
+    for(let option of QuestionTwoOptions)
+    {
+        option.addEventListener("click",questionTwo);
+    }
+
+
+}
+
+function addEventsInQuestionThree(){
+    const QuestionThreeOptions = document.querySelectorAll("[data-container='Container For Questions And Options'] li");
+
+    for(let option of QuestionThreeOptions)
+    {
+        option.addEventListener("click",questionThree);
+    }
+    
+}
+
+function addEventsInQuestionFour(){
+    const QuestionFourOptions = document.querySelectorAll("[data-container='Container For Questions And Options'] li");
+
+    for(let option of QuestionFourOptions)
+    {
+        option.addEventListener("click",questionFour);
+    }
+}
+
+function addEventsInQuestionFive(){
+    const QuestionFiveOptions = document.querySelectorAll("[data-container='Container For Questions And Options'] li");
+
+    for(let option of QuestionFiveOptions)
+    {
+        option.addEventListener("click",questionFive);
+    }
+}
+
+
+function addEventsInQuestionSix(){
+    const QuestionSixOptions = document.querySelectorAll("[data-container='Container For Questions And Options'] li");
+
+    for(let option of QuestionSixOptions)
+    {
+        option.addEventListener("click",questionSix);
+    }
+}
+
+function addEventsInQuestionSeven(){
+    const QuestionSevenOptions = document.querySelectorAll("[data-container='Container For Questions And Options'] li");
+
+    for(let option of QuestionSevenOptions)
+    {
+        option.addEventListener("click",questionSeven);
+    }
+}
+
+function addEventsInQuestionEight(){
+    const QuestionEightOptions = document.querySelectorAll("[data-container='Container For Questions And Options'] li");
+
+    for(let option of QuestionEightOptions)
+    {
+        option.addEventListener("click",questionEight);
+    }
+}
+
+function addEventsInQuestionNine(){
+    const QuestionNineOptions = document.querySelectorAll("[data-container='Container For Questions And Options'] li");
+
+    for(let option of QuestionNineOptions)
+    {
+        option.addEventListener("click",questionNine);
+    }
+}
+
+function addEventsInQuestionTen(){
+    const QuestionTenOptions = document.querySelectorAll("[data-container='Container For Questions And Options'] li");
+
+    for(let option of QuestionTenOptions)
+    {
+        option.addEventListener("click",questionTen);
+    }
+}
+
+
+export {addEventsInQuestionOne,addEventsInQuestionTwo,addEventsInQuestionThree,addEventsInQuestionFour,addEventsInQuestionFive,addEventsInQuestionSix,addEventsInQuestionSeven,addEventsInQuestionEight,addEventsInQuestionNine,addEventsInQuestionTen };
