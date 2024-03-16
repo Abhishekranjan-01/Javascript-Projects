bccb947c894eb6e17781b1b6d79fa6de9dea6576
Abhishekranjan-01 <abhishekranjan7991@outlook.com>
2024-03-16 16:54:08 +0530

    Updates In Projects

--- 1-Level/Recipe-App/filters/nutrientsCloseButton.js
+++
@@ -1,7 +0,0 @@
-const searchByNutrients = document.getElementById('search-by-nutrients');
-
-console.log(searchByNutrients);
-
-searchByNutrients.addEventListener('click',()=>{
-    nutrientsForm.classList.toggle('hidden');
-})
